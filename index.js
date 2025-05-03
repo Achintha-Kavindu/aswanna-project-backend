a9bb1750af8cb48ca5a8166f227ffe0cd23e480f
Achintha-Kavindu <achinthakavinduakpk@gmail.com>
2025-05-03 22:29:00 +0530

    mongodb conected sucsefully

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+const connectionString = process.env.MONGODB_URL;
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("data base connected");
+  })
+  .catch(() => {
+    console.log("data base error");
+  });
 
 app.listen(5000, (req, res) => {
   console.log("server is runing on port 5000");
